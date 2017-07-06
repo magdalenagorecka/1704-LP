19ad714b1aa8abaf99e40735bb30151f8b51e49a
Magdalena Górecka <goorecka.magdalena@gmail.com>
2017-07-06 15:38:06 +0200

    Meta + script
    Dodano skrypt smooth scroll
    Poprawki RWD

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 
-
+// **** top scroll ********/
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
@@ -10,8 +9,24 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+$(document).ready(function(){
+
+  //****** smooth scroll*********//
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });//animate
+    } // End if
+  });
+});
